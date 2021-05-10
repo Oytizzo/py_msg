307001e9d7edb5ce820bc23409457b2152f51efa
Oytizzo <oytizzo@gmail.com>
2021-05-10 18:25:39 +0600

    Added Employee class and property decorator

--- src/app.py
+++ src/app.py
@@ -1,1 +1,27 @@
-print("Hello world! jello.")
+# python oop
+
+class Employee:
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+    
+    @property
+    def email(self):
+        return self.first + '.' + self.last + '@email.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+emp_1 = Employee('John', 'Smith', 40000)
+emp_2 = Employee('Devid', 'Hobarmann', 30000)
+
+print(emp_1)
+print(emp_2)
+print(emp_1.fullname())
+print(emp_1.email)
+
+emp_1.first = 'lisa'
+print(emp_1.fullname())
+print(emp_1.email)
